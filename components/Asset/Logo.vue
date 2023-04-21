f76cf05513c2e4dd4d400f2a7346e97cb99f2e98
ParallelDEX <126363788+ParallelDEX@users.noreply.github.com>
2023-04-21 18:36:51 +0200

    Update Logo.vue

--- components/Asset/Logo.vue
+++ components/Asset/Logo.vue
@@ -7,7 +7,22 @@ export default {
   name: 'SvgComponent',
   data() {
     return {
-   <svg height="65" stroke-miterlimit="1" version="1.1" viewBox="0 0 288 300" width="65" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:vectornator="http://vectornator.io" xmlns:xlink="http://www.w3.org/1999/xlink" style="fill-rule: nonzero; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round;"><defs></defs><g id="_x35_997047a-69c2-432d-836c-d299ff0ddb69" vectornator:layerName="_x35_997047a-69c2-432d-836c-d299ff0ddb69"><path d="M119.218 79.0275L119.218 220.973C119.218 229.415 132.372 229.415 132.372 220.973L132.372 79.0275C132.372 70.5854 119.218 70.5854 119.218 79.0275ZM167.613 79.0275L167.613 220.973C167.613 229.415 180.767 229.415 180.767 220.973L180.767 79.0275C180.767 70.5854 167.613 70.5854 167.613 79.0275Z" fill="#ffffff" fill-rule="nonzero" opacity="1" stroke="none"></path></g></svg>
+   svgCode: `<svg version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+       viewBox="0 0 300 300" style="enable-background:new 0 0 300 300;" xml:space="preserve">
+      <style type="text/css">
+        .st0{fill:url(#SVGID_1_);}
+        .st1{fill:#FFFFFF;}
+      </style>
+      <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="-806.008" y1="690.7227" x2="-805.008" y2="690.7227" gradientTransform="matrix(271.8923 126.7855 126.7855 -271.8923 131543.125 290058.0625)">
+        <stop  offset="0" style="stop-color:#3616AC"/>
+        <stop  offset="1" style="stop-color:#E09BED"/>
+      </linearGradient>
+      <rect class="st0" width="300" height="300"/>
+      <g id="_x35_997047a-69c2-432d-836c-d299ff0ddb69" transform="matrix(0.9816394080243864,0,0,0.9816394080243864,3.0903597561459364,103.17580697761967)">
+        <path class="st1" d="M118.3-24.6V120c0,8.6,13.4,8.6,13.4,0V-24.6C131.7-33.2,118.3-33.2,118.3-24.6z M167.6-24.6V120
+        c0,8.6,13.4,8.6,13.4,0V-24.6C181-33.2,167.6-33.2,167.6-24.6z"/>
+      </g>
+    </svg>`
     };
   }
 };
