cf318208e1b8a9b464d712cdaf8d1dec4776d5f5
ParallelDEX <126363788+ParallelDEX@users.noreply.github.com>
2023-04-22 18:05:03 +0200

    Update Topbar.vue

--- components/Layout/Topbar.vue
+++ components/Layout/Topbar.vue
@@ -5,7 +5,6 @@ const modalStore = useModalStore()
 const walletStore = useWalletStore()
 const ninjaPassStore = useNinjaPassStore()
 const confetti = useConfetti()
-const router = useRouter()
 
 const props = defineProps({
   isSidebarOpen: Boolean
@@ -67,10 +66,7 @@ function handleShowNinjaPassModal() {
     }"
   >
     <div
-      class="cursor-pointer pl-6 lg:pr-6 lg:border-r flex items-center"
-      @click="router.push({ name: 'index' })"
-    >
-      <AssetLogo class="w-auto h-6 lg:h-[30px]" alt="Parallel" />
+ alt="Parallel" />
     </div>
     <div class="flex-1 px-2 lg:px-6 flex justify-end lg:justify-between">
       <div
